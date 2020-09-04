0b56898a047b27ed9165457ccc1d7a5eacdaa7a1
facuhump <facu2409@hotmail.com>
2020-09-04 20:26:35 -0300

    ya creado boton de login de google con nuestras credenciales

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Routes from './components/Routes'
+import GoogleBtn from './components/components/GoogleBtn';
 
 function App() {
   return (
+    <div>
       <Routes />
+      <GoogleBtn />
+    </div>
   );
 }
 
