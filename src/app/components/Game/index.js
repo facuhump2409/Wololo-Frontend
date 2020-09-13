c7406eedabf8850b55dc84b4154535f0180c7d13
Maxi-F <maxifeldman@hotmail.com>
2020-09-13 19:09:31 -0300

    changed some styles and auth is saved in local storage

--- src/app/components/Game/index.js
+++ src/app/components/Game/index.js
@@ -22,28 +22,30 @@ const Game = () => {
   }
 
   return (
-    <div className='game-container'>
-      <img 
-        src={process.env.PUBLIC_URL + '/Buenos_aires.webp'} 
-        alt='provincia' 
-        useMap='#gameMap'
-        onLoad={handleOnImageLoad}
-        />
+    <div>
 
-      <map name="gameMap">
-        <area shape='rect'
-         coords={`0, 0, ${imageDimensions.dimensions.width/2}, ${imageDimensions.dimensions.height}`} 
-         alt='Player 1'
-         onClick={handleClick1} 
-         />
-        <area shape='rect'
-         coords={`${imageDimensions.dimensions.width/2}, 0, ${imageDimensions.dimensions.width}, ${imageDimensions.dimensions.height}`} 
-         alt='Player 1'
-         onClick={handleClick2} 
-         />
-      </map>
+      <div className='d-flex justify-content-center'>
+        <img 
+          src={process.env.PUBLIC_URL + '/Buenos_aires.webp'} 
+          alt='provincia' 
+          useMap='#gameMap'
+          onLoad={handleOnImageLoad}
+          />
 
-      <h1>{test}</h1>
+        <map name="gameMap">
+          <area shape='rect'
+          coords={`0, 0, ${imageDimensions.dimensions.width/2}, ${imageDimensions.dimensions.height}`} 
+          alt='Player 1'
+          onClick={handleClick1} 
+          />
+          <area shape='rect'
+          coords={`${imageDimensions.dimensions.width/2}, 0, ${imageDimensions.dimensions.width}, ${imageDimensions.dimensions.height}`} 
+          alt='Player 1'
+          onClick={handleClick2} 
+          />
+        </map>
+      </div>
+        <h1>{test}</h1>
     </div>
   )
 }
