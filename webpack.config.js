c0f5c55504656c19539f63ccd7a11a55be653ef1
Maxi-F <maxifeldman@hotmail.com>
2020-09-28 17:46:01 -0300

    added configs to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,75 @@
 /*eslint-env node*/
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'lib'),
-		filename: 'index.js',
-		library: 'SVGMap',
-		libraryTarget: 'commonjs2'
+module.exports = (env, options) => {
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, './'),
+			filename: 'index.js',
+		},
+		performance: {
+			hints: false // Disable assets limit
+		},
+		devServer: {
+			historyApiFallback: true,
+			proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite: {'^/api' : ''}
+      }
+		}
 	},
-	module: {
-		rules: [{
-			test: /\.jsx?/,
-			exclude: /node_modules/,
-			options: {
-				presets: ['react']
+		module: {
+			rules: [{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				use: [
+					'babel-loader',
+					{
+						loader: 'eslint-loader',
+						options: {
+							emitWarning: options.mode === 'development', // Avoid to block compilation when ESLint error
+						}
+					}
+				],
+			}, {
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'style-loader'
+				}, {
+					loader: 'css-loader'
+				}, {
+					loader: 'sass-loader'
+				}]
+			}, {
+				test: /\.html$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'html-loader',
+					options: {
+						minimize: true
+					}
+				}]
 			},
-			use: [
-				'babel-loader',
 				{
-					loader: 'eslint-loader'
-				}
-			],
-		}, {
-			test: /\.scss$/,
-			exclude: /node_modules/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				'css-loader',
-				'sass-loader'
-			]
-		}]
-	},
-	resolve: {
-		extensions: ['.json', '.js', '.jsx'],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: 'index.css'
-		})
-	],
-	externals: {
-		'react': 'commonjs react'
-	}
+					test: /\.css$/,
+					use: [
+						'style-loader',
+						'css-loader'
+					]
+				}]
+		},
+		resolve: {
+			extensions: ['.json', '.js', '.jsx'],
+		},
+		plugins: [
+			new HtmlWebPackPlugin({
+				template: './public/index.html',
+				filename: './index.html'
+			})
+		]
+	};
 };
