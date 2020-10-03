a82433304e6e25289144321d075e1059afad997b
facuhump2409 <facu2409@hotmail.com>
2020-10-03 00:00:12 -0300

    arreglados paths que no iban a ningun lado

--- src/app/components/Modals/townModal.js
+++ src/app/components/Modals/townModal.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import { Modal } from 'rsuite';
 
-function TownModal() {
-    return (
-      <p> Aca irian todos los datos </p>
-    );
+class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            backdrop: false,
+            show: false
+        };
+        this.close = this.close.bind(this);
+        this.open = this.open.bind(this);
+    }
+    close() {
+        this.setState({ show: false });
+    }
+    open() {
+        this.setState({ show: true });
+    }
+    render() {
+        const { backdrop, show } = this.state;
+        return (
+            <div className="modal-container">
+                <span>Backdrop: </span>
+
+                <RadioGroup
+                    name="radioList"
+                    inline
+                    value={backdrop}
+                    onChange={value => {
+                        this.setState({ backdrop: value });
+                    }}
+                >
+                    <Radio value={true}>true</Radio>
+                    <Radio value={false}>false</Radio>
+                    <Radio value="static">static</Radio>
+                </RadioGroup>
+                <hr />
+                <ButtonToolbar>
+                    <Button onClick={this.open}> Open</Button>
+                </ButtonToolbar>
+
+                <Modal backdrop={backdrop} show={show} onHide={this.close}>
+                    <Modal.Header>
+                        <Modal.Title>Modal Title</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Paragraph />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.close} appearance="primary">
+                            Ok
+                        </Button>
+                        <Button onClick={this.close} appearance="subtle">
+                            Cancel
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
 }
 
-export default TownModal;
+ReactDOM.render(<Demo />);
