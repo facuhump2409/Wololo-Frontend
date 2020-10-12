1f7b6a2c30a382961f108402488434d23e2c7fee
facuhump2409 <facu2409@hotmail.com>
2020-10-12 14:26:14 -0300

    subo primeros pasos para la integracion pero sigue el error de undef

--- src/app/App.js
+++ src/app/App.js
@@ -3,11 +3,24 @@ import {Provider,connect} from 'react-redux';
 import Routes from './components/Routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/index.css'
-import {store} from '../redux/store'
+import {history, store} from '../redux/store'
+import AdminView from "./components/Admin/adminView";
+import jsonServerProvider from "ra-data-json-server";
+import {Admin} from "react-admin";
+import NotFound from "./components/Admin/notFound";
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
   return (
     <Provider store={store}>
+        <Admin
+            dataProvider={dataProvider}
+            history={history}
+            title="Wololo Admin"
+            catchAll={NotFound}
+        >
+
+        </Admin>
       <Routes/>
     </Provider>
   );
