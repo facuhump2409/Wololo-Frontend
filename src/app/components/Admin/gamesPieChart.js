7cad2bd51f42bc1ee184cba1f83a241cca070cd5
facuhump2409 <facu2409@hotmail.com>
2020-10-13 00:46:20 -0300

    ya alimentandose de los datos de back

--- src/app/components/Admin/gamesPieChart.js
+++ src/app/components/Admin/gamesPieChart.js
@@ -5,6 +5,8 @@ import {VictorySharedEvents} from "victory-shared-events";
 
 function GamesPieChart(props) {
     console.log("data dentro de pie",props.data)
+    const [cancelled,finished,newGames,onGoing] = props.data ? [props.data.gamesCanceled + 1, props.data.gamesFinished + 1,props.data.gamesNew + 1,props.data.gamesOnGoing + 1] : [1,1,1,1]
+    //TODO sacar esos mas 1 estan hardcodeados porque sino back solo devuelve 0's
     return (
         <div>
             <svg viewBox="0 0 850 350">
@@ -34,7 +36,7 @@ function GamesPieChart(props) {
                         }
                     }]}
                 >
-                    <g transform={"translate(150, 50)"}>
+                    <g transform={"translate(250, 50)"}>
                         <VictoryBar name="bar"
                                     width={300}
                                     standalone={false}
@@ -44,20 +46,20 @@ function GamesPieChart(props) {
                                         labels: {fontSize: 10}
                                     }}
                                     data={[
-                                        {x: "Cancelled", y: 2}, {x: "Finished", y: 3}, {x: "New Games", y: 5}, {x: "On Going", y: 4}
+                                        {x: "Cancelled", y: cancelled}, {x: "Finished", y: finished}, {x: "New Games", y: newGames}, {x: "On Going", y: onGoing}
                                     ]}
                                     labels={["Cancelled", "Finished", "New Games", "On Going"]}
                                     labelComponent={<VictoryLabel y={290}/>}
                         />
                     </g>
-                    <g transform={"translate(0, -75)"}>
+                    <g transform={"translate(0, -100)"}>
                         <VictoryPie name="pie"
                                     width={250}
                                     colorScale={"qualitative"}
                                     standalone={false}
                                     style={{ labels: {fontSize: 10, padding: 10}}}
                                     data={[
-                                        {x: "Cancelled", y: 1}, {x: "Finished", y: 4}, {x: "New Games", y: 5}, {x: "On Going", y: 7}
+                                        {x: "Cancelled", y: cancelled}, {x: "Finished", y: finished}, {x: "New Games", y: newGames}, {x: "On Going", y: onGoing}
                                     ]}
                         />
                     </g>
