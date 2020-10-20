1efe04422159fea04988bbbdae1184275405d28c
facuhump2409 <facu2409@hotmail.com>
2020-10-20 00:06:33 -0300

    updateo el readme y subo primer approach para correr todo con un .sh

---
+++ start_app.sh
@@ -0,0 +1,5 @@
+mkdir tp-tacs
+cd tp-tacs
+git clone https://github.com/Maxi-F/wololo-react.git && git clone https://github.com/Tp-Tacs/TP-TACS.git
+cd wololo-react
+docker-compose build && docker-compose up -d
