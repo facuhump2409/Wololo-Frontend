55f42d73d413c5382da072b0dfa60359a4439774
facuhump2409 <facu2409@hotmail.com>
2020-11-01 16:53:06 -0300

    vuelvo a puerto estatico por ahora

--- WololoSI/src/index.ts
+++ WololoSI/src/index.ts
@@ -1,7 +1,15 @@
+// import { server } from "./server/server";
+// // import serverConfig from "./serverconfig.json";
+// var app = require('express')();
+// const serverPort: number = app.address().port;
+
+// server.listen(serverPort, () => {
+//     console.log(`Server running on port ${serverPort}...`);
+// });
 import { server } from "./server/server";
-// import serverConfig from "./serverconfig.json";
-var app = require('express')();
-const serverPort: number = app.address().port;
+import serverConfig from "./serverconfig.json";
+
+const serverPort: number = serverConfig.port;
 
 server.listen(serverPort, () => {
     console.log(`Server running on port ${serverPort}...`);
