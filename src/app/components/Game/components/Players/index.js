4ae120b4b7c8629cbdb5a28bf33d7d62c87f9aff
Maxi-F <maxifeldman@hotmail.com>
2020-11-22 14:35:16 -0300

    added turn and currentuser logic to user list

--- src/app/components/Game/components/Players/index.js
+++ src/app/components/Game/components/Players/index.js
@@ -4,6 +4,8 @@ import { COLORS } from '../../../../constants';
 import './index.css'
 
 function Players({ turn, currentUser, players, colors }) {
+  const isCurrentUser = (player) => currentUser === player.id
+  const isItsTurn = (player) => turn === player.id
   return (
     <ul>
       {players.map(player => (
@@ -11,10 +13,11 @@ function Players({ turn, currentUser, players, colors }) {
           <span 
             className='dot' 
             style={{
-              backgroundColor: currentUser === player.id ? COLORS.current : colors[player.id]
+              backgroundColor: isCurrentUser(player) ? COLORS.current : colors[player.id],
+              opacity: isItsTurn(player) ? 1 : 0.3
             }}
           />
-          <li className={`font ${turn === player.id && 'itsTurn'}`}>{player.username}</li>
+          <li className={`font ${isItsTurn(player) && 'itsTurn'}`}>{`${player.username}${isCurrentUser(player) ? ' (you)' : ''}`}</li>
         </div>
         ))
       }
