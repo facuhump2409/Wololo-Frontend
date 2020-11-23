55cd5eaa402cd5011583afdd6873ccbdd7562701
CimimUxMaio <cimimuxmaio@gmail.com>
2020-11-22 21:47:26 -0300

    Subo nuevo start_app script

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,46 @@
-mkdir tp-tacs
-cd tp-tacs
-git clone https://github.com/Maxi-F/wololo-react.git && git clone https://github.com/Tp-Tacs/TP-TACS.git
-cd wololo-react
-docker-compose build && docker-compose up -d
+#!/bin/bash
+
+echo "------------------------------------------------------------------"
+echo "\n\nSetting up directories...\n"
+
+project_name="tp-tacs"
+if [ -d "$project_name" ]
+then
+    echo "$project_name already exists, using it instead"
+else
+    echo "creating $project_name directory"
+    mkdir "$project_name"
+fi
+
+cd "$project_name"
+
+echo "------------------------------------------------------------------"
+echo "\n\nCloning repos...\n"
+
+reuising_message="already cloned. Reusing it..."
+frontend_name="wololo-react"
+backend_name="TP-TACS"
+
+if [ -d "$frontend_name" ] 
+then 
+    echo "$frontend_name $reuising_message" 
+else
+    git clone "https://github.com/Maxi-F/$frontend_name.git"
+fi
+
+if [ -d "$backend_name" ] 
+then
+    echo "$backend_name $reuising_message"
+else
+    git clone "https://github.com/Tp-Tacs/$backend_name.git"
+fi
+
+cd "$frontend_name"
+
+echo "------------------------------------------------------------------"
+echo "\n\nBuilding application...\n"
+sudo docker-compose build
+
+echo "------------------------------------------------------------------"
+echo "\n\nStarting application...\n"
+sudo docker-compose up -d
